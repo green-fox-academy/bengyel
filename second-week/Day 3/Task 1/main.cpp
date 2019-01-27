e59d930ab64cfcbcd860607b91b642750f7f4bfe
bengyel <bengyelg@gmail.com>
2019-01-27 14:45:32 +0100

    Drawing

--- second-week/Day 3/Task 1/main.cpp
+++ second-week/Day 3/Task 1/main.cpp
@@ -22,10 +22,5 @@ int main() {
     }catch (char const* c){
         std::cout << c << std::endl;
     }
-    // Create a function that takes a number
-    // divides ten with it,
-    // and prints the result.
-    // It should print "fail" if the parameter is 0
-
     return 0;
 }
