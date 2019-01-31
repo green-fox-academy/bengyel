b55dd26aa0af7f1be268870e74612028bce1d62f
bengyel <bengyelg@gmail.com>
2019-01-31 13:21:02 +0100

    counter

---
+++ Third week/Day 4/Task1/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+// Write a recursive function that takes one parameter: n and counts down from n.
+void counter(int count);
+int main() {
+    int n;
+    n = 7;
+
+    counter(n);
+
+    return 0;
+}
+void counter(int count)
+{
+    if (count >= 1)
+    {
+    counter(count - 1);
+    std::cout << count << std::endl;
+    }
+};
